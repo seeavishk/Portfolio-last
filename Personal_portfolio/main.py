63de9fb37674be75035af730e86bd89bfff75391
aviral <129966113+seeavishk@users.noreply.github.com>
2024-07-14 12:28:37 +0530

    Update main.py

--- Personal_portfolio/main.py
+++ Personal_portfolio/main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import google.generativeai as genai
-
+ 
 api_key = st.secrets["GOOGLE_API_KEY"]
+# api_key =""
 genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
